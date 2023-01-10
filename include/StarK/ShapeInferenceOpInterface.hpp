0ac7069b9987a1c293eac1eb4bfaf5e1918257f1
Dhamodharan-AlphaICS <g.dhamodharan@alphaics.com>
2023-01-10 13:30:28 +0530

    bug found on StarK-opt calling

--- include/StarK/ShapeInferenceOpInterface.hpp
+++ include/StarK/ShapeInferenceOpInterface.hpp
@@ -1,16 +1,4 @@
 
-
-//===---- ShapeInferenceOpInterface.hpp - Definition for ShapeInference ---===//
-//
-// Copyright 2019-2020 The IBM Research Authors.
-//
-// =============================================================================
-//
-// This file contains the declarations of the shape inference interfaces defined
-// in ShapeInferenceInterface.td.
-//
-//===----------------------------------------------------------------------===//
-
 #pragma once
 
 #include "mlir/IR/BuiltinOps.h"
